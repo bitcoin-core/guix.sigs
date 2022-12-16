7400a921c672c601c036fd624d613a1dbb0c359d
MarcoFalke <falke.marco@gmail.com>
2022-12-16 12:20:07 +0100

    ci: Return file lists from check()

--- contrib/touched-files-check/src/main.rs
+++ contrib/touched-files-check/src/main.rs
@@ -1,8 +1,10 @@
-fn check(touched_files: &str) -> Result<(), String> {
+use std::collections::{HashMap, HashSet};
+
+fn check(touched_files: &str) -> Result<(Vec<&str>, HashSet<&str>), String> {
     let attestation_regex = regex::Regex::new("^([^/]+/[^/]+/[^/]+.SHA256SUMS)(|.asc)$").unwrap();
-    let mut attestations = std::collections::HashMap::new();
+    let mut attestations = HashMap::new();
     let builder_key_regex = regex::Regex::new("^(builder-keys/[^/]+.gpg)$").unwrap();
-    let mut builder_keys = Vec::new();
+    let mut builder_keys = HashSet::new();
     for line in touched_files.lines() {
         let (status, file) = {
             let mut l = line.split_whitespace();
@@ -25,7 +27,7 @@ fn check(touched_files: &str) -> Result<(), String> {
                 ));
             }
         } else if let Some(path) = builder_key_regex.captures(file) {
-            builder_keys.push(path.get(1).unwrap().as_str());
+            assert!(builder_keys.insert(path.get(1).unwrap().as_str()));
             if status != "A" && status != "M" {
                 return Err(format!(
                     "File status for builder key is not 'A' (for add) or 'M' (for modified): '{status}' '{file}'"
@@ -35,14 +37,14 @@ fn check(touched_files: &str) -> Result<(), String> {
             return Err(format!("Added unknown file '{file}'"));
         }
     }
-    for (path, asc) in attestations {
+    for (path, asc) in &attestations {
         if asc.len() != 2 {
             return Err(format!(
                 "Missing SHA256SUMS.asc or SHA256SUMS file in {path}"
             ));
         }
     }
-    Ok(())
+    Ok((attestations.into_keys().collect(), builder_keys))
 }
 
 fn main() {
@@ -60,7 +62,7 @@ fn main() {
 
 #[test]
 fn test_check() {
-    assert_eq!(check("M README.md"), Ok(()));
+    assert_eq!(check("M README.md"), Ok((vec![], HashSet::new())));
     assert_eq!(
         check("B 22.0/user/all.SHA256SUMS").unwrap_err(),
         "File status for attestation is not 'A' (for add): 'B' '22.0/user/all.SHA256SUMS'"
@@ -75,11 +77,14 @@ fn test_check() {
     );
     assert_eq!(
         check("A 22.0/user/all.SHA256SUMS\nA 22.0/user/all.SHA256SUMS.asc"),
-        Ok(())
+        Ok((vec!["22.0/user/all.SHA256SUMS"], HashSet::new()))
     );
     assert_eq!(
         check("B builder-keys/user.gpg").unwrap_err(),
         "File status for builder key is not 'A' (for add) or 'M' (for modified): 'B' 'builder-keys/user.gpg'",
     );
-    assert_eq!(check("M builder-keys/user.gpg"), Ok(()));
+    assert_eq!(
+        check("M builder-keys/user.gpg"),
+        Ok((vec![], HashSet::from(["builder-keys/user.gpg"])))
+    );
 }
